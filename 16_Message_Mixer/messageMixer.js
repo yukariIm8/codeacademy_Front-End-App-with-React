79961949055ac8964f021f5f0a92046f2b2b72f2
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-04-13 00:02:17 -0700

    refactor: create an object to export the module.

--- 16_Message_Mixer/messageMixer.js
+++ 16_Message_Mixer/messageMixer.js
@@ -1,63 +1,66 @@
-function countCharacter(inputString, inputCharacter) {
-    let count = 0;
-    let string = inputString.toLowerCase();
-    let character = inputCharacter.toLowerCase();
-      for (let i = 0; i < string.length; i++) {
-        if (string[i] === character) {
-           count++;
-        }
+// Create a MessageMixer object to export the module as an object.
+const MessageMixer = {};
+
+// Attach the each function as a property to MessageMixer object.
+MessageMixer.countCharacter = function(inputString, inputCharacter) {
+  let count = 0;
+  let string = inputString.toLowerCase();
+  let character = inputCharacter.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === character) {
+         count++;
       }
-    return count; 
-  };
-  
-  function capitalizeFirstCharacterOfWords(string) {
-    let arr = string.split(" ");  
-      for (let i = 0; i < arr.length; i++) {  
-        let word = arr[i];
-          arr[i] = word[0].toUpperCase() + word.substring(1); 
-      }
-    return arr.join(" "); 
-  };
-  
-  
-  function reverseWord(word) {
-    return word.split("").reverse().join("");
-  };
-  
-  function reverseAllWords(sentence) {
-    let words = sentence.split(" ");
-      for (let i = 0; i < words.length; i++) {
-        words[i] = reverseWord(words[i]);
-      }
-     return words.join(" ");
-  };
-  
-  
-  function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
-    return string.replace(toBeReplaced, replaceWith);
-  };
-  
-  
-  function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
-    return string.split(toBeReplaced).join(replaceWith);
-  };
-  
-  function encode(string) {
-    let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
-      for (let key in replacementObject) {
-        string = replaceAllOccurrences(string, key, replacementObject[key]); 
-      }	
-      return string;
-  };
-  
-  
-  function displayMessage() {
-    console.log(countCharacter("What is the color of the sky?", "t"));
-    console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-    console.log(reverseWord("What is the color of the sky?"));
-    console.log(reverseAllWords("What is the color of the sky?"));
-    console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-    console.log(encode("What is the color of the sky?"));
-  }
-  
-  displayMessage();
+    }
+  return count; 
+};
+
+MessageMixer.capitalizeFirstCharacterOfWords = function(string) {
+  let arr = string.split(" ");  
+    for (let i = 0; i < arr.length; i++) {  
+      let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1); 
+    }
+  return arr.join(" "); 
+};
+
+
+MessageMixer.reverseWord = function(word) {
+  return word.split("").reverse().join("");
+};
+
+MessageMixer.reverseAllWords = function(sentence) {
+  let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = MessageMixer.reverseWord(words[i]);
+    }
+   return words.join(" ");
+};
+
+
+MessageMixer.replaceFirstOccurence = function(string, toBeReplaced, replaceWith) {
+  return string.replace(toBeReplaced, replaceWith);
+};
+
+
+MessageMixer.replaceAllOccurrences = function(string, toBeReplaced, replaceWith) {
+  return string.split(toBeReplaced).join(replaceWith);
+};
+
+MessageMixer.encode = function(string) {
+  let replacementObject = { "a": "@", "s": "$", "i": "!", "o":"0" };
+    for (let key in replacementObject) {
+      string = MessageMixer.replaceAllOccurrences(string, key, replacementObject[key]); 
+    }	
+    return string;
+};
+
+MessageMixer.palindrome = function(str) {
+  return `${str} ${MessageMixer.reverseWord(str)}`;
+}
+
+MessageMixer.pigLatin = function(sentence, character) {
+  return sentence.split(' ').join(character + ' ');
+}
+
+// Export MessageMixer object.
+export default MessageMixer;
