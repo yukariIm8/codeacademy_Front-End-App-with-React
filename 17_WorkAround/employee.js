46f22ea0f9df815abfdaca588d88cfae74eb7252
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-04-14 00:09:13 -0700

    refactor: create an object to export as a module.

--- 17_WorkAround/employee.js
+++ 17_WorkAround/employee.js
@@ -1,4 +1,7 @@
-let salary = 100000;
+// Create an object to export the module as an object.
+let Employee = {
+  salary: 100000 
+};
 
 let payGrades = {
   entryLevel: { taxMultiplier: .05, benefits: ['health'], minSalary: 10000, maxSalary: 49999 },
@@ -6,46 +9,35 @@ let payGrades = {
   seniorLevel: { taxMultiplier: .2, benefits: ['health', 'housing', 'wellness', 'gym'], minSalary: 100000, maxSalary: 200000 }
 };
 
-function getCadre() {
-  if (salary >= payGrades.entryLevel.minSalary && salary <= payGrades.entryLevel.maxSalary) {
+Employee.getCadre = function() {
+  if (Employee.salary >= payGrades.entryLevel.minSalary && Employee.salary <= payGrades.entryLevel.maxSalary) {
     return 'entryLevel';
-  } else if (salary >= payGrades.midLevel.minSalary && salary <= payGrades.midLevel.maxSalary) {
+  } else if (Employee.salary >= payGrades.midLevel.minSalary && Employee.salary <= payGrades.midLevel.maxSalary) {
     return 'midLevel';
   } else return 'seniorLevel';
-}
+};
 
-function calculateTax() {
-  return payGrades[getCadre()].taxMultiplier * salary;
-}
+Employee.calculateTax = function() {
+  return payGrades[Employee.getCadre()].taxMultiplier * Employee.salary;
+};
 
-function getBenefits() {
-  return payGrades[getCadre()].benefits.join(', ');
-}
+Employee.getBenefits = function() {
+  return payGrades[Employee.getCadre()].benefits.join(', ');
+};
 
-function calculateBonus() {
-  return .02 * salary;
-}
+Employee.calculateBonus = function() {
+  return .02 * Employee.salary;
+};
 
-function reimbursementEligibility() {
+Employee.reimbursementEligibility = function() {
   let reimbursementCosts = { health: 5000, housing: 8000, wellness: 6000, gym: 12000 };
   let totalBenefitsValue = 0; 
-  let employeeBenefits = payGrades[getCadre()].benefits;
+  let employeeBenefits = payGrades[Employee.getCadre()].benefits;
   for (let i = 0; i < employeeBenefits.length; i++) {
     totalBenefitsValue += reimbursementCosts[employeeBenefits[i]];
   }
   return totalBenefitsValue;
-}
+};
 
-function getEmployeeInformation(inputSalary) {
-  salary = inputSalary;
-  console.log('Cadre: ' + getCadre());
-  console.log('Tax: ' + calculateTax());
-  console.log('Benefits: ' + getBenefits());
-  console.log('Bonus: ' + calculateBonus());
-  console.log('Reimbursement Eligibility: ' + reimbursementEligibility() + '\n');
-}
-
-getEmployeeInformation(10000);
-getEmployeeInformation(50000);
-getEmployeeInformation(100000);
-
+// Export the Employee object by using export default.
+export default Employee;
